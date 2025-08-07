32f9b2715cf3ee6a435be139a83be1cc1bfb502e
chasel0720 <chasel0720@gmail.com>
2025-08-07 19:52:14 +0800

    L14

--- Exercise/Lessons/L14_longest_common_prefix.cs
+++ Exercise/Lessons/L14_longest_common_prefix.cs
@@ -8,10 +8,32 @@ public class L14_longest_common_prefix : LessonBase<string[], string>
         (["flower","flow","flight"],"fl"),
         (["dog","racecar","car"],"")
     ];
-    // public override bool NeedToRunSingle => true;
+    // public override bool NeedToRunSingle => false;
 
-    public override string Run(string[] testCase)
+    public override string Run(string[] strs)
     {
-        throw new NotImplementedException();
+        var result = "";
+        int index = 0;
+        var prefix = "";
+        while (true)
+        {
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (index > strs[i].Length - 1)
+                {
+                    return result;
+                }
+                if (i == 0)
+                {
+                    prefix = strs[i][index].ToString();
+                }
+                else if (prefix != strs[i][index].ToString())
+                {
+                    return result;
+                }
+            }
+            index++;
+            result += prefix;
+        }
     }
 }
