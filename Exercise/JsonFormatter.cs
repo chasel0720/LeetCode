9bc67e0ddc6afbe30a4d407c34cf6756cf48d834
chasel0720 <chasel0720@gmail.com>
2025-08-12 11:40:35 +0800

    update jsonFormatter

--- Exercise/JsonFormatter.cs
+++ Exercise/JsonFormatter.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Exercise;
@@ -7,7 +9,10 @@ public static class JsonFormatter
     public static string ToJsonString<T>(this T value)
     {
         if (value == null) return "null";
-
+        if (value.GetType().IsValueType)
+        {
+            return SerializeTuple(value);
+        }
         if (!value.GetType().IsClass) return $"\"{value}\"";
 
         var options = new JsonSerializerOptions
@@ -19,4 +24,46 @@ public static class JsonFormatter
 
         return JsonSerializer.Serialize(value, options);
     }
+
+    static string SerializeTuple(object tuple)
+    {
+        var sb = new StringBuilder("(");
+        var type = tuple.GetType();
+
+        var fields = type.GetFields();
+        for (int i = 0; i < fields.Length; i++)
+        {
+            var value = fields[i].GetValue(tuple);
+            sb.Append(FormatValue(value));
+            if (i < fields.Length - 1) sb.Append(", ");
+        }
+
+        sb.Append(')');
+        return sb.ToString();
+    }
+
+    static string FormatValue(object value)
+    {
+        if (value == null) return "null";
+
+        return value switch
+        {
+            Array array => FormatArray(array),
+            ITuple tuple => SerializeTuple(tuple),
+            _ => value.ToString()
+        };
+    }
+
+    static string FormatArray(Array arr)
+    {
+        var sb = new StringBuilder("[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sb.Append(arr.GetValue(i));
+            if (i < arr.Length - 1) sb.Append(',');
+        }
+        sb.Append(']');
+        return sb.ToString();
+    }
+
 }
